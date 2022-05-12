75e12ae35e00c83d84f78e1f4559132601f5ac32
jintajinta <jinta219@gmail.com>
2022-05-12 16:27:57 +0900

    add Mein.kt

--- app/src/main/java/com/example/myapplication/MainActivity.kt
+++ app/src/main/java/com/example/myapplication/MainActivity.kt
@@ -1,11 +1,27 @@
 package com.example.myapplication
 
+
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.TextView
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+
+    }
+
+    fun plusnum(view: View) {
+        val countTextView = findViewById<TextView>(R.id.textView)
+        val currentnum = Integer.parseInt(countTextView.text.toString())
+        countTextView.setText((currentnum+1).toString())
+    }
+
+    fun resetnum(view: View) {
+        val countTextView = findViewById<TextView>(R.id.textView)
+        countTextView.setText("0")
     }
 }
