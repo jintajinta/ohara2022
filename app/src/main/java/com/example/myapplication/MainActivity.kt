0235f28111b2b1a07d9a0e272494793e3eae1942
jintajinta <jinta219@gmail.com>
2022-06-09 14:39:13 +0900

    dentaku2 完成

--- app/src/main/java/com/example/myapplication/MainActivity.kt
+++ app/src/main/java/com/example/myapplication/MainActivity.kt
@@ -5,8 +5,6 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
 import android.widget.TextView
-import java.lang.Integer.parseInt
-import android.util.Log
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -15,6 +13,8 @@ class MainActivity : AppCompatActivity() {
 
 
         val  Calculatedvalue= findViewById<TextView>(R.id.calculatedvalue)
+        val  Tmpvalue= findViewById<TextView>(R.id.tmpvalue)
+        val  Culsym= findViewById<TextView>(R.id.culsym)
         val  Clear=findViewById<Button>(R.id.clear)
         val  Allclear=findViewById<Button>(R.id.allclear)
         val  Signchange=findViewById<Button>(R.id.signchange)
@@ -37,98 +37,126 @@ class MainActivity : AppCompatActivity() {
         var count= 0F
         var tmp= 0F
         var curoperator=""
-        var mulcount=0
 
         Add.setOnClickListener {
-            tmp=calculation(curoperator,tmp,count)
-            Calculatedvalue.setText(tmp.toString())
-            count=0F
+            if (count!=0F) {
+                tmp = calculation(curoperator, tmp, count)
+                Tmpvalue.setText("")
+                Calculatedvalue.setText(tmp.toString())
+                count = 0F
+            }
             curoperator="add"
+            Culsym.setText(calculationsymbol(curoperator))
         }
         Sub.setOnClickListener {
-            tmp=calculation(curoperator,tmp,count)
-            Calculatedvalue.setText(tmp.toString())
-            count=0F
-            curoperator="sub"
+            if (count!=0F) {
+                tmp = calculation(curoperator, tmp, count)
+                Tmpvalue.setText("")
+                Calculatedvalue.setText(tmp.toString())
+                count = 0F
+            }
+            curoperator = "sub"
+            Culsym.setText(calculationsymbol(curoperator))
+
         }
         Mul.setOnClickListener {
             if (count!=0F) {
                 tmp = calculation(curoperator, tmp, count)
+                Tmpvalue.setText("")
                 Calculatedvalue.setText(tmp.toString())
                 count = 0F
             }
             curoperator="mul"
+            Culsym.setText(calculationsymbol(curoperator))
         }
         Div.setOnClickListener {
             if (count!=0F) {
                 tmp = calculation(curoperator, tmp, count)
+                Tmpvalue.setText("")
                 Calculatedvalue.setText(tmp.toString())
                 count = 0F
             }
             curoperator="div"
+            Culsym.setText(calculationsymbol(curoperator))
         }
         Equal.setOnClickListener {
             tmp=calculation(curoperator,tmp,count)
+            Tmpvalue.setText("")
             Calculatedvalue.setText(tmp.toString())
             count=0F
         }
         Num0.setOnClickListener {
             count *= 10
+            Tmpvalue.setText(tmp.toString())
             Calculatedvalue.setText(count.toString())
         }
         Num00.setOnClickListener {
             count *= 100
+            Tmpvalue.setText(tmp.toString())
             Calculatedvalue.setText(count.toString())
         }
         Num1.setOnClickListener {
             count = count*10+1
+            Tmpvalue.setText(tmp.toString())
             Calculatedvalue.setText(count.toString())
         }
         Num2.setOnClickListener {
             count = count*10+2
+            Tmpvalue.setText(tmp.toString())
             Calculatedvalue.setText(count.toString())
         }
         Num3.setOnClickListener {
             count = count*10+3
+            Tmpvalue.setText(tmp.toString())
             Calculatedvalue.setText(count.toString())
         }
         Num4.setOnClickListener {
             count = count*10+4
+            Tmpvalue.setText(tmp.toString())
             Calculatedvalue.setText(count.toString())
         }
         Num5.setOnClickListener {
             count = count*10+5
+            Tmpvalue.setText(tmp.toString())
             Calculatedvalue.setText(count.toString())
         }
         Num6.setOnClickListener {
             count = count*10+6
+            Tmpvalue.setText(tmp.toString())
             Calculatedvalue.setText(count.toString())
         }
         Num7.setOnClickListener {
             count = count*10+7
+            Tmpvalue.setText(tmp.toString())
             Calculatedvalue.setText(count.toString())
         }
         Num8.setOnClickListener {
             count = count*10+8
+            Tmpvalue.setText(tmp.toString())
             Calculatedvalue.setText(count.toString())
         }
         Num9.setOnClickListener {
             count = count*10+9
+            Tmpvalue.setText(tmp.toString())
             Calculatedvalue.setText(count.toString())
         }
         Clear.setOnClickListener {
             count=0F
+            Tmpvalue.setText(tmp.toString())
             Calculatedvalue.setText(count.toString())
         }
         Allclear.setOnClickListener {
             count=0F
             tmp=0F
+            Tmpvalue.setText("")
             Calculatedvalue.setText(count.toString())
             curoperator=""
+            Culsym.setText(calculationsymbol(curoperator))
         }
         Signchange.setOnClickListener {
             if (count!=0F) {
                 count *= -1
+                Tmpvalue.setText(tmp.toString())
                 Calculatedvalue.setText(count.toString())
             }
         }
@@ -146,6 +174,20 @@ class MainActivity : AppCompatActivity() {
             tmp/count
         }else{
             count
+        }
+    }
+
+    private fun calculationsymbol(curoperator:String): String {
+        return if(curoperator=="add"){
+            "＋"
+        }else if(curoperator=="sub"){
+            "－"
+        }else if(curoperator=="mul"){
+            "×"
+        }else if(curoperator=="div"){
+            "÷"
+        }else{
+            ""
         }
     }
 }
